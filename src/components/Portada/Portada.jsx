0e480e05efb6a6912a94ce9d9e89eca25bb9d6a0
Carmelo Rubiolo <melo_rubiolo@hotmail.com>
2023-01-23 19:00:01 -0300

    first commit

---
+++ src/components/Portada/Portada.jsx
@@ -0,0 +1,19 @@
+import "./Portada.css"
+
+
+const Portada = () => {
+    return(
+        
+
+        <>
+            <div className="img-fondo">
+                <img className="tittle-portada"src="https://fontmeme.com/permalink/230120/b9f43a7c2792c919fb11a8b0b373d199.png" alt="fuente-de-minions" border="0"/>
+                <img className="tittle-nombre"src="https://fontmeme.com/permalink/230120/bed7c313571c47d1645ec1b00b1b3f3a.png" alt="fuente-de-minions" border="0"/>
+                <img className="img-portada" src="../../../imagenes/yellow-arrow.png" />
+                <img className="img-portada2" src="../../../imagenes/yellow-arrow.png" />
+            </div>
+        </>
+    )
+}
+
+export default Portada
