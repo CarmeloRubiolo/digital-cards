0a1180f002aa24ac413dda7af5e0a7893e51d5ec
Carmelo Rubiolo <melo_rubiolo@hotmail.com>
2023-01-23 19:52:07 -0300

    cambio en imagen de flecha - portada

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -7,10 +7,12 @@ const Portada = () => {
 
         <>
             <div className="img-fondo">
-                <img className="tittle-portada"src="https://fontmeme.com/permalink/230120/b9f43a7c2792c919fb11a8b0b373d199.png" alt="fuente-de-minions" border="0"/>
-                <img className="tittle-nombre"src="https://fontmeme.com/permalink/230120/bed7c313571c47d1645ec1b00b1b3f3a.png" alt="fuente-de-minions" border="0"/>
-                <img className="img-portada" src="../../../imagenes/yellow-arrow.png" />
-                <img className="img-portada2" src="../../../imagenes/yellow-arrow.png" />
+                <div className="flex-container">
+                    <img className="tittle-portada"src="https://fontmeme.com/permalink/230120/b9f43a7c2792c919fb11a8b0b373d199.png" alt="fuente-de-minions" border="0"/>
+                    <img className="tittle-nombre"src="https://fontmeme.com/permalink/230120/bed7c313571c47d1645ec1b00b1b3f3a.png" alt="fuente-de-minions" border="0"/>
+                    <img className="img-portada bounce-top" src="../../../imagenes/flecha-abajo.png" alt="img flecha" />
+                    <img className="img-portada2 bounce-top" src="../../../imagenes/flecha-abajo.png"  alt="img flecha"/>
+                </div>
             </div>
         </>
     )
